4f57176d1b0fe44cdb93e5a3e7d1785e1147ce18
sachin <sachinmanoharan100@gmail.com>
2024-06-27 17:41:51 +0530

    pdf download

--- client/src/Pages/Project.js
+++ client/src/Pages/Project.js
@@ -1,10 +1,12 @@
-import { useContext, useEffect, useRef, useState } from 'react';
-import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
-import Editor from '../Components/editorComp';
-import { SidebarContext } from '../context/sidebarContext';
-import { projectDetails, projectUpdate } from '../services/project';
-import { storyIndexing } from '../utils/story';
-import { tabWidth } from '../utils/tabWidth';
+import { useContext, useEffect, useRef, useState } from "react";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+import Editor from "../Components/editorComp";
+import { SidebarContext } from "../context/sidebarContext";
+import { projectDetails, projectUpdate } from "../services/project";
+import { storyIndexing } from "../utils/story";
+import { tabWidth } from "../utils/tabWidth";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
 function Project() {
   const { id, type } = useParams();
@@ -30,10 +32,10 @@ function Project() {
   };
 
   const updateProjectById = async (payload, setLoading) => {
-    if (isRead === 'view') return setLoading(false);
+    if (isRead === "view") return setLoading(false);
     await ejInstance.current.save();
-    let stories =  storyIndexing(payload?.body?.blocks)
-    const data = await projectUpdate(id, {...payload, stories}, setLoading);
+    let stories = storyIndexing(payload?.body?.blocks);
+    const data = await projectUpdate(id, { ...payload, stories }, setLoading);
     setEditorData(data?.body);
     setProjectData(data);
     setIsMount(true);
@@ -43,25 +45,39 @@ function Project() {
   useEffect(() => {
     getProjectById();
     return () => {
-      setHeaderName('');
+      setHeaderName("");
     };
   }, []);
+
+  console.log(editorData, "editorData");
+
+  const downloadPDF = () => {
+    html2canvas(document.querySelector(`#main-editor`)).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF("p", "mm", "a4");
+      const imgProps = pdf.getImageProperties(imgData);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+      pdf.save(`${projectData?.name}.pdf`);
+    });
+  };
 
   return (
     <>
       <div
         className={`container mx-auto sm:pt-2 ${
-          isRead === 'view' && 'bg-white'
+          isRead === "view" && "bg-white"
         }`}
         style={{
           paddingLeft: tabWidth ? 0 : 100,
           paddingRight: tabWidth ? 0 : 100,
-          maxHeight: '90vh',
-          overflowY: 'scroll',
+          maxHeight: "90vh",
+          overflowY: "scroll",
         }}
       >
-        <div style={{ display: 'flex', justifyContent: 'end' }}>
-          {isRead === 'edit' && (
+        <div style={{ display: "flex", justifyContent: "end" }}>
+          {isRead === "edit" && (
             <button
               disabled={isLoding}
               onClick={() => {
@@ -79,16 +95,25 @@ function Project() {
               className="mgq-bg-primary mb-3 radius-3 hover:mgq-bg-primary-dark text-white py-2 px-4 rounded text-sm focus:outline-none focus:shadow-outline mr-3"
               type="button"
             >
-              {isLoding ? 'Saving' : 'Save'}
+              {isLoding ? "Saving" : "Save"}
             </button>
           )}
-          <button
+          {/* <button
             className="bg-gray-500 mb-3 radius-3 hover:bg-gray-700  text-white py-2 px-4 rounded text-sm focus:outline-none focus:shadow-outline mr-3"
             type="button"
             onClick={() => setShowIndexesModal(true)}
           >
             Indexes
-          </button>
+          </button> */}
+          {type === "view" && (
+            <button
+            className="mgq-bg-primary mb-3 radius-3 hover:mgq-bg-primary-dark text-white py-2 px-4 rounded text-sm focus:outline-none focus:shadow-outline mr-3"
+              type="button"
+              onClick={downloadPDF}
+            >
+              Download PDF
+            </button>
+          )}
           <button
             disabled={isReadLoading}
             onClick={async () => {
@@ -102,7 +127,7 @@ function Project() {
                   setIsReadLoading
                 );
                 window.location.href = `/projects/${id}/${
-                  type === 'view' ? 'edit' : 'view'
+                  type === "view" ? "edit" : "view"
                 }`;
               }, [200]);
             }}
@@ -110,10 +135,10 @@ function Project() {
             type="button"
           >
             {isReadLoading
-              ? 'Loading...'
-              : isRead === 'edit'
-              ? 'Preview'
-              : 'Edit'}
+              ? "Loading..."
+              : isRead === "edit"
+              ? "Preview"
+              : "Edit"}
           </button>
 
           {/* {isRead === 'edit' && <button
@@ -127,53 +152,102 @@ function Project() {
             Clear Editor
           </button>} */}
         </div>
-
+        {/* <div id="main-editor"> */}
         <Editor
           isMount={isMount}
-          isRead={isRead === 'edit' ? false : true}
+          isRead={isRead === "edit" ? false : true}
           ejInstance={ejInstance}
           id="main-editor"
           setEditorData={setEditorData}
           editorData={editorData}
         />
-        {showIndexesModal ?
-          <div className="relative z-10 p-5" aria-labelledby="modal-title" role="dialog" aria-modal={showIndexesModal}>
-            <div className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity' />
+        {/* </div> */}
+        {showIndexesModal ? (
+          <div
+            className="relative z-10 p-5"
+            aria-labelledby="modal-title"
+            role="dialog"
+            aria-modal={showIndexesModal}
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
             <div className="fixed inset-0 z-10 overflow-y-auto">
-              <div className={`${showIndexesModal ? 'flex' : 'hidden'} min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0`} >
+              <div
+                className={`${
+                  showIndexesModal ? "flex" : "hidden"
+                } min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0`}
+              >
                 <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                   <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                     <div>
-                      {projectData?.stories?.map(story => (
-                        story.scene_id?
-                        <div key={story.scene_id} className="mb-4">
-                          <div>
-                            <span className="text-xl font-black">{story.scene_number}</span>
-                            <span className="text-sm"> has <span className='font-medium'>{story.charecters_count} {story.charecters_count===1?"character":"characters"}</span></span>
-                          {Object.keys(story.charecters).map(key => (
-                            <div className='ml-4' key={key}>
-                              <span className='font-bold'>{key}</span> has <span className='font-bold'>{story.charecters[key]}</span> {story.charecters[key]===1?"dialogue":"dialogues"}
+                      {projectData?.stories?.map((story) =>
+                        story.scene_id ? (
+                          <div key={story.scene_id} className="mb-4">
+                            <div>
+                              <span className="text-xl font-black">
+                                {story.scene_number}
+                              </span>
+                              <span className="text-sm">
+                                {" "}
+                                has{" "}
+                                <span className="font-medium">
+                                  {story.charecters_count}{" "}
+                                  {story.charecters_count === 1
+                                    ? "character"
+                                    : "characters"}
+                                </span>
+                              </span>
+                              {Object.keys(story.charecters).map((key) => (
+                                <div className="ml-4" key={key}>
+                                  <span className="font-bold">{key}</span> has{" "}
+                                  <span className="font-bold">
+                                    {story.charecters[key]}
+                                  </span>{" "}
+                                  {story.charecters[key] === 1
+                                    ? "dialogue"
+                                    : "dialogues"}
+                                </div>
+                              ))}
+                              {story?.character_array.length !==
+                              Object.keys(story?.charecters).length ? (
+                                <div className="ml-4">
+                                  <span className="font-bold">
+                                    {story?.character_array?.map(
+                                      (character, index) =>
+                                        Object.keys(story?.charecters).includes(
+                                          character
+                                        )
+                                          ? ""
+                                          : `${character}${
+                                              index ===
+                                              story.character_array.length - 1
+                                                ? ""
+                                                : ", "
+                                            }`
+                                    )}
+                                  </span>{" "}
+                                  have <span className="font-bold">0</span>{" "}
+                                  dialogues
+                                </div>
+                              ) : (
+                                ""
+                              )}
                             </div>
-                          ))}
-                          {story?.character_array.length !== Object.keys(story?.charecters).length ?
-                          <div className='ml-4'>
-                            <span className='font-bold'>
-                            {story?.character_array?.map((character, index) => (
-                              Object.keys(story?.charecters).includes(character)?"":`${character}${index===story.character_array.length-1?"":", "}`
-                            ))}
-                            </span> have <span className='font-bold'>0</span> dialogues
                           </div>
-                          :""}
+                        ) : (
+                          <div className="flex justify-center">
+                            Nothing is indexed yet, please save or refresh the
+                            project
                           </div>
-                        </div>
-                        : <div className="flex justify-center">
-                          Nothing is indexed yet, please save or refresh the project
-                        </div>
-                      ))}
+                        )
+                      )}
                     </div>
                     <div className="my-3 flex justify-end">
                       <div>
-                        <button type="button" className="bg-red-500 radius-3 rounded-md border hover:bg-red-600 px-4 py-2 font-medium text-white text-sm focus:outline-none focus:shadow-outline" onClick={() => setShowIndexesModal(false)}>
+                        <button
+                          type="button"
+                          className="bg-red-500 radius-3 rounded-md border hover:bg-red-600 px-4 py-2 font-medium text-white text-sm focus:outline-none focus:shadow-outline"
+                          onClick={() => setShowIndexesModal(false)}
+                        >
                           Cancel
                         </button>
                       </div>
@@ -183,7 +257,7 @@ function Project() {
               </div>
             </div>
           </div>
-          : null}
+        ) : null}
       </div>
     </>
   );
